d358fcfe286f2e968ce8e156f2d971016651da4f
Pham Minh Long <longpml3421@gmail.com>
2024-05-27 22:06:18 +0700

    Updated Dockerfile

--- docker_run.sh
+++ docker_run.sh
@@ -1,23 +1,19 @@
-# Display
-export XAUTH=/tmp/.docker.xauth
+#!/bin/bash
+USERNAME=noetic
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-docker run -it --rm --privileged --name noetic_desktop_full \
-    -v $(pwd):/root/$(basename $(pwd)) -w /root/$(basename $(pwd)) \
+docker run -it --rm \
+    --user $USERNAME \
+    --name noetic_desktop_full \
+    --network host --ipc host \
+    -v $(pwd):/home/$USERNAME/$(basename $(pwd)) -w /home/$USERNAME/$(basename $(pwd)) \
+    --privileged \
     --gpus all \
-    --env="DISPLAY=$DISPLAY" --network host\
-    --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="DISPLAY=$DISPLAY" --env="QT_X11_NO_MITSHM=1" \
+    --volume="$XSOCK:$XSOCK:rw" \
     --env="XAUTHORITY=$XAUTH" --volume="$XAUTH:$XAUTH" \
-    robot-navigate:latest\
+    robot-navigate:latest \
     bash
-
-# VNC (TODO)
-# docker run -it --rm --privileged --name noetic_desktop_full \
-#     -v $(pwd):/ws -w /ws \
-#     --gpus all \
-#     --shm-size 1g --network host\
-#     --volume ~/.bash_history:/home/vscode/.bash_history \
-#     -p 6080:6080 \
-#     robot-navigate:latest \
-#     bash
