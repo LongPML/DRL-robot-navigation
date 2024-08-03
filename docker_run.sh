2fb21e3a707713f3e0556468c70fbea8941d8f3b
Pham Minh Long <longpml3421@gmail.com>
2024-08-03 16:50:22 +0700

    Update git ignore and docker run

--- docker_run.sh
+++ docker_run.sh
@@ -12,8 +12,9 @@ docker run -it \
     -v $(pwd):/home/$USERNAME/$(basename $(pwd)) -w /home/$USERNAME/$(basename $(pwd)) \
     --privileged \
     --gpus all \
-    --env="DISPLAY=$DISPLAY" --env="QT_X11_NO_MITSHM=1" \
-    --volume="$XSOCK:$XSOCK:rw" \
-    --env="XAUTHORITY=$XAUTH" --volume="$XAUTH:$XAUTH" \
+    --volume=$XSOCK:$XSOCK:rw \
+    --volume=$XAUTH:$XAUTH:rw \
+    --env="XAUTHORITY=${XAUTH}" \
+    --env="DISPLAY" \
     navigate-robot:noetic-py3.8.10-torch1.10.0-cu111 \
     bash
